efbc29613e42820dff06d847d94028314204bded
Chetan Kokal <chetankokal27@gmail.com>
2023-04-30 17:54:37 +0530

    Create a custom hook to fetch the games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
@@ -6,3 +6,5 @@ export default axios.create({
     key: "9ea6c1f64870415f80faf9781370d602",
   },
 });
+
+export { CanceledError };
