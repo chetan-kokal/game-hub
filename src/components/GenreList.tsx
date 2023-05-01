973e360c1e38654e022d115e9b679a1f1fbdba87
Chetan Kokal <chetankokal27@gmail.com>
2023-05-01 20:06:15 +0530

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,28 @@
+import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data: genres } = useGenres();
 
   return (
-    <ul>
-      {genres.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
-      ))}
-    </ul>
+    <List>
+      {genres.map((genre) => {
+        return (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                src={getCroppedImageUrl(genre.image_background)}
+                boxSize="32px"
+                borderRadius={8}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        );
+      })}
+    </List>
   );
 };
 
